ce378fbfaa94c74cfda8d42909e5ee4c7ddbae84
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-03-25 18:52:55 -0300

    Products registered in stock V0.1
    Register of products in stock.

---
+++ product_in_stock/src/main/java/stock/Entities/Product/product_Id.java
@@ -0,0 +1,38 @@
+package stock.Entities.Product;
+
+public class product_Id {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+        
+        return quantity * price;
+         
+    }
+
+    public void addProducts(int quantity){
+        
+        this.quantity += quantity;
+
+    }
+
+    public void removeProducts(int quantity){
+
+        this.quantity -= quantity;
+
+    }
+
+    public String toString(){
+
+        return name
+            + ", $ "
+            + String.format("%.2f", price)
+            + ", "
+            + quantity
+            + " units, Total: $ "
+            + String.format("%.2f", totalValueInStock());
+
+    }
+}

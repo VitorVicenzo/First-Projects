96e5117114224969c89738ecdb7bdd281e622877
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-03-26 18:11:57 -0300

    Encapsulating the bank.

---
+++ banco_Central/src/main/entities/Bank.java
@@ -0,0 +1,12 @@
+package main.entities;
+
+public class Bank {
+
+    private final double IOF = 0.06;
+    private double deposit;
+    private double withdraw;
+    private String name;
+    private int number_Account;
+    private int senha;
+}
+ 
